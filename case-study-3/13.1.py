6d5fe93c0023934cae207925d5e53b7e3a3dd13b
aidan mcbride <aidanmcbride@outlook.com>
2019-09-05 16:47:37 -0600

    Rename 13.1.1.py to 13.1.py

--- case-study-3/13.1.py
+++ case-study-3/13.1.py
@@ -1,4 +1,4 @@
-# case-study-3/13.1.1.py
+# case-study-3/13.1.py
 
 sample_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
