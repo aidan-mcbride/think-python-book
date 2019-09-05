2dea75779dde252e006a03006698286cab8f1430
aidan mcbride <aidanmcbride@outlook.com>
2019-09-05 09:47:51 -0600

    Exercise 12.2.1 add function to print sets of anagrams
    Add function that prints any list of words longer than one word, since a list of one word means there are no anagrams.
    At this point my program satisfies the requirements of the exercise.

--- exercises/12.2.1.py
+++ exercises/12.2.1.py
@@ -23,10 +23,7 @@ for each word in word list:
     append the word to the list for that key
 """
 
-# fin = open('exercises/words.txt')
-
-### TESTING ###
-fin = ['cat', 'rat', 'art', 'tack']
+fin = open('exercises/words.txt')
 
 def word_to_tuple(word):
     """
@@ -34,18 +31,33 @@ def word_to_tuple(word):
     returns a tuple of each letter in that word
     sorted alphabetically
     """
-    word = word.strip()
     # since strings are sequences of letters
     # `sorted` will automatically convert a string
     # to a list, then sort it
     word = tuple(sorted(word))
     return word
 
+def print_anagrams(anagrams):
+    """
+    takes a dictionary of lists of words keyed
+    to tuples of the letters in those words
+    prints any list of words with more than one word in it.
+    """
+    for letter_set in anagrams:
+        words = anagrams.get(letter_set)
+        if len(words) > 1:
+            print(words)
 
 def anagrams(word_list):
+    """
+    takes a list of words
+    returns a dictionary containing lists of words
+    keyed to tuples of the letters in those words
+    """
     output = dict()
 
     for word in word_list:
+        word = word.strip()
         letters = word_to_tuple(word)
         # add letters as key to output dict
         # if not present already
@@ -53,7 +65,7 @@ def anagrams(word_list):
         # append word to list at key
         output[letters].append(word)
 
-    print(output)
+    return output
 
-#### TEST ####
-anagrams(fin)
+
+print_anagrams(anagrams(fin))
