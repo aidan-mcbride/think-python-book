3437bb6fc5dbb7e674ccd64e0a913895a3fbdd95
aidan mcbride <aidanmcbride@outlook.com>
2019-09-05 18:24:03 -0600

    Exercise 13.2 add function to create dictionary of words and frequencies

--- case-study-3/13.2.py
+++ case-study-3/13.2.py
@@ -3,12 +3,13 @@
 
 import string
 
+
 def import_book(filename):
     """
     takes a book text file's filename
     returns the book as a string, sans the gutenberg header
     """
-    with open('case-study-3/{}'.format(filename), 'r') as f:
+    with open("case-study-3/{}".format(filename), "r") as f:
         book_string = str()
         # book starts on line 34
         for i in range(33):
@@ -16,8 +17,9 @@ def import_book(filename):
         for line in f:
             book_string += line
 
-        # for testing, only return first 200 characters
-        return book_string[:200]
+        # for testing, only return first 300 words
+        return book_string[:300]
+
 
 def parse_book(text):
     """
@@ -28,12 +30,35 @@ def parse_book(text):
     returns a list of words
     """
     # remove punctuation
-    no_punc = text.translate(text.maketrans('', '', string.punctuation))
+    no_punc = text.translate(text.maketrans("", "", string.punctuation))
 
     parsed = no_punc.lower().strip().split()
     return parsed
 
 
-plato = import_book('pg55201.txt')
+def word_frequency(word_list):
+    """
+    takes a list of words
+    creates a dictionary where the key is a word
+    and the value is the frequency of that word
+    returns that dictionary
+    """
+    # histogram function basically
+    hist = dict()
+    for word in word_list:
+        hist[word] = hist.get(word, 0) + 1
+    return hist
 
-print(parse_book(plato))
+
+#####################
+#### RUN PROGRAM ####
+#####################
+
+plato = import_book("pg55201.txt")
+
+word_list = parse_book(plato)
+word_freq = word_frequency(word_list)
+
+print("total words: {}".format(len(word_list)))
+print(word_freq)
+print("different words: {}".format(len(word_freq)))
